b93c98a46ac3ea993132d0bd95b39808c444b908
Bogdan Barbu <69798890+bodi-cmd@users.noreply.github.com>
2023-01-07 16:24:48 +0200

    Update app.config.js

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
     PRODUCTION: true,
-    SERVER_ADRESS:"http://localhost:8080",
+    SERVER_ADRESS:"https://groups.herokuapp.com",
     DEFAULT_TOKEN:"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJmMjAwYzUzMy1jYTQzLTQ4NmItOTUxYS0xYjk0ZTg2NGFmYjUiLCJleHAiOjE2NzIwNzk5MDMsImlhdCI6MTY3MjA2MTkwM30.K-8dPShoLgf769gjQkdnZNHcXkEi6xonbG2qlEi8526769SHBGiSzARPB3fVFHDOdGyxUcu8A798YqMB_5iGuQ",
     DEFAULT_AVATAR:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
 }
